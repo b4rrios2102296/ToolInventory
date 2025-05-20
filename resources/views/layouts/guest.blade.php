4c97011ac33565d47986a959140545f44883a91b
Diego Ávila Barrios <2102296@utrivieramaya.edu.mx>
2025-05-19 20:12:17 -0500

    first good syles implemented

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -1,6 +1,4 @@
 <!DOCTYPE html>
-
-
 <html lang="es">
 <head>
     <meta charset="UTF-8">
@@ -8,23 +6,37 @@
     <title>{{ config('app.name') }} - @yield('title')</title>
     @livewireStyles
     <script src="https://cdn.tailwindcss.com"></script>
+    <style>
+        body {
+            width: 100%;
+            min-height: 100vh;
+            margin: 0;
+            padding: 0;
+            background-image: url('/Images/workshop-4863393_1280.jpg');
+            background-size: cover;
+            background-position: center;
+            background-repeat: no-repeat;
+            background-attachment: fixed;
+            background-color: #000; /* Respaldo si la imagen no carga */
+            position: relative; /* Para el posicionamiento del pseudo-elemento */
+        }
+        
+        body::before {
+            content: '';
+            position: absolute;
+            top: 0;
+            left: 0;
+            width: 100%;
+            height: 100%;
+            background-color: rgba(0, 0, 0, 0.8);
+            z-index: -1; /* Para que no cubra el contenido */
+        }
+    </style>
 </head>
 <body>
-<div class="min-h-screen flex items-center justify-center p-4">
+    <div class="min-h-screen flex items-center justify-center p-4 relative z-10">
         {{ $slot }}
     </div>
     @livewireScripts
 </body>
 </html>
-<style>
-    body {
-  width: 100%;
-  height: 1080px;
-  background: rgba(0,0,0,1);
-  opacity: 1;
-  position: absolute;
-  top: 0px;
-  left: 0px;
-  overflow: hidden;
-    }
-</style>
