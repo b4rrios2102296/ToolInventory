5b0fafad7d3d19b0632a848eba12b7e473ddd8d7
Diego Ávila Barrios <134070827+b4rrios2102296@users.noreply.github.com>
2025-05-24 10:59:49 -0500

    first good style changes to the dasboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,9 +1,1 @@
 @extends('layouts.app')
-
-
-@section('content')
-    <flux:main>
-        <flux:heading size="xl" level="1">Bienvenido, {{ Auth::user()->nombre }}</flux:heading>
-        <flux:separator variant="subtle" />
-    </flux:main>
-@endsection
