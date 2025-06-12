68429e5fd35c2ac7469145ab32c48db3ef51fffb
Diego Ávila Barrios <134070827+b4rrios2102296@users.noreply.github.com>
2025-06-12 16:41:39 -0500

    adawdwadawda

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -27,18 +27,16 @@
         <flux:sidebar.toggle class="lg:hidden" icon="x-mark" />
 
         <!-- Logo / Header -->
-        <div style="padding-bottom: 0; margin-top: -100px; margin-left: -90px;">
+        <div style="padding-bottom: 0; margin-top: -100px; margin-left: -120px;">
             <img src="{{ asset('Images/grand-velas-riviera-maya-mexico-logo.svg') }}"
-                style="width: 150; height: 240px;" class="mx-auto" />
+                style="width: 100px; height: 205px;" class="mx-auto" />
         </div>
-
 
         <div class="flex flex-col items-center justify-center space-y-2 mb-8 mt-2 text-center">
-            <div style="padding-bottom: -50px; margin-top: -150px; padding-left: 10px;">
-                <img src="{{ asset('Images/Group 64.svg') }}" style="width: 180px; margin-right: 50px;" />
+            <div style="padding-bottom: -50px; margin-top: -150px;">
+                <img src="{{ asset('Images/Group 64.svg') }}" style="width: 200px; margin-right: 50px;" />
             </div>
         </div>
-
 
         <!-- Navigation -->
         <flux:navlist style="padding-bottom: -50; margin-top: -90px;">
