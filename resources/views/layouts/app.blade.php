425f777d6383ba2e7cf2a9291180041581eb0576
Diego Ávila Barrios <134070827+b4rrios2102296@users.noreply.github.com>
2025-06-16 10:40:46 -0500

    now the app name sows on the window of the browser

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <title>{{ config('app.name') }} - Dashboard</title>
+    <title>{{ config('app.name') }} </title>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 
     <!-- Fuentes y estilos -->
